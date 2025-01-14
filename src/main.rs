563bd33e7c501fbd88110fa5e5f74319d71433a4
Felipe Coury <felipe.coury@gmail.com>
2025-01-14 11:19:14 -0300

    Add configuration option for active and logging-only servers
    * Add `active_server` and `logging_only_servers` variables
    * Update `upstreams` initialization to include logging-only servers
    * Use struct literal syntax for `load_balancer::LB` to include `active_server` and `load_balancer`

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,10 @@ fn main() {
     let mut my_server = Server::new(None).unwrap();
     my_server.bootstrap();
 
-    let mut upstreams =
-        LoadBalancer::try_from_iter(["1.1.1.1:443", "1.0.0.1:443", "127.0.0.1:343"]).unwrap();
+    let active_server = "1.1.1.1:443".to_string();
+    let logging_only_servers = vec!["1.0.0.1:443".to_string(), "127.0.0.1:343".to_string()];
+
+    let mut upstreams = LoadBalancer::try_from_iter(logging_only_servers.clone()).unwrap();
 
     let hc = TcpHealthCheck::new();
     upstreams.set_health_check(hc);
@@ -20,7 +22,10 @@ fn main() {
     let background = background_service("health check", upstreams);
     let upstreams = background.task();
 
-    let mut lb = http_proxy_service(&my_server.configuration, load_balancer::LB(Arc::clone(&upstreams)));
+    let mut lb = http_proxy_service(&my_server.configuration, load_balancer::LB {
+        load_balancer: Arc::clone(&upstreams),
+        active_server: active_server.clone(),
+    });
     lb.add_tcp("0.0.0.0:6188");
     println!("Listening on: 0.0.0.0:6188");
 
